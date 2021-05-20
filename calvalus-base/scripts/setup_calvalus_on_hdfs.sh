e89ac9047331c9a64d174a694dae00fa9bc9f2bb
Declan Dunne <d.dunne@ucc.ie>
2021-05-20 15:53:29 +0100

    Use -f flag for all 'hdfs dfs -copyFromLocal'

--- calvalus-base/scripts/setup_calvalus_on_hdfs.sh
+++ calvalus-base/scripts/setup_calvalus_on_hdfs.sh
@@ -8,16 +8,16 @@ cd /build/usr/share/java/
 java -jar calvalus/calvalus-distribution/cpt.jar -e --deploy calvalus/calvalus-bundle/*.jar calvalus-2.19-SNAPSHOT
 
 # Add GDAL JNI support
-hdfs dfs -copyFromLocal gdal.jar /calvalus/software/1.0/calvalus-2.19-SNAPSHOT
+hdfs dfs -copyFromLocal -f gdal.jar /calvalus/software/1.0/calvalus-2.19-SNAPSHOT
 
 # install snap-bundle
 java -jar calvalus/calvalus-distribution/cpt.jar -e --deploy calvalus/snap-bundle/snap-all.jar snap-7.0.4cv-SNAPSHOT
 
 # add snap python support
-hdfs dfs -copyFromLocal calvalus/snap-python/org-esa-snap-snap-python.jar /calvalus/software/1.0/snap-7.0.4cv-SNAPSHOT
+hdfs dfs -copyFromLocal -f calvalus/snap-python/org-esa-snap-snap-python.jar /calvalus/software/1.0/snap-7.0.4cv-SNAPSHOT
 
 # add bundle-descriptor.xml - to help register product file formats in portal
-hdfs dfs -copyFromLocal calvalus/snap-bundle/resources/bundle-descriptor.xml /calvalus/software/1.0/snap-7.0.4cv-SNAPSHOT
+hdfs dfs -copyFromLocal -f calvalus/snap-bundle/resources/bundle-descriptor.xml /calvalus/software/1.0/snap-7.0.4cv-SNAPSHOT
 
 # confirm copy:
 hdfs dfs -ls /calvalus/software/1.0
