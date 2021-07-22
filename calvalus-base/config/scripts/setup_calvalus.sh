fd6fe70a4f860769ca3cb7991e8edcc261c56737
Declan Dunne <d.dunne@ucc.ie>
2021-07-22 22:11:05 +0100

    Add missing SNAP built-in bundle descriptor

--- calvalus-base/config/scripts/setup_calvalus.sh
+++ calvalus-base/config/scripts/setup_calvalus.sh
@@ -16,4 +16,7 @@ java -jar calvalus/calvalus-distribution/cpt.jar -e --deploy calvalus/snap-bundl
 # add snap python support
 hdfs dfs -copyFromLocal -f calvalus/snap-python/org-esa-snap-snap-python.jar /calvalus/software/1.0/snap-8.0.3.3cv
 
+# add snap built-in bundle descriptor
+hdfs dfs -copyFromLocal -f calvalus/snap-bundle/resources/bundle-descriptor.xml /calvalus/software/1.0/snap-8.0.3.3cv
+
 echo "Finished installing Calvalus on HDFS"
