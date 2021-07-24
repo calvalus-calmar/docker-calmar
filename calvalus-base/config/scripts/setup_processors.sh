e7e375f492e9d0e45eb531617ec4bfacf81d30e0
Declan Dunne <d.dunne@ucc.ie>
2021-07-24 22:35:04 +0100

    Minor text changes for the S1 SAR L2 OCN processor

--- calvalus-base/config/scripts/setup_processors.sh
+++ calvalus-base/config/scripts/setup_processors.sh
@@ -4,5 +4,5 @@ echo "Installing demo processor to HDFS..."
 hdfs dfs -mkdir -p /calvalus/software/1.0
 
 # install processor
-hdfs dfs -copyFromLocal processor-bundles/S1-L2-preprocess-1.0 /calvalus/software/1.0/
-hdfs dfs -copyFromLocal /build/usr/share/java/calvalus/calmar/calmar-ocean-wind-energy-op-0.1.0-SNAPSHOT.jar /calvalus/software/1.0/S1-L2-preprocess-1.0/
+hdfs dfs -copyFromLocal processor-bundles/S1-L2-OCN-preprocess-1.0 /calvalus/software/1.0/
+hdfs dfs -copyFromLocal /build/usr/share/java/calvalus/calmar/calmar-ocean-wind-energy-op-0.1.0-SNAPSHOT.jar /calvalus/software/1.0/S1-L2-OCN-preprocess-1.0/
