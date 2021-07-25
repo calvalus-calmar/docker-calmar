c5fb919d5a879a5e30cb32089a146a31b411e2a8
Declan Dunne <d.dunne@ucc.ie>
2021-07-25 21:55:14 +0100

    Use -f flag with 'hdfs dfs -copyFromLocal'

--- calvalus-base/config/scripts/setup_processors.sh
+++ calvalus-base/config/scripts/setup_processors.sh
@@ -4,8 +4,8 @@ echo "Installing demo processors to HDFS..."
 hdfs dfs -mkdir -p /calvalus/software/1.0
 
 # install processors
-hdfs dfs -copyFromLocal processor-bundles/S1-L1-GRD-Terrain-Correction-1.0 /calvalus/software/1.0/
-hdfs dfs -copyFromLocal /build/usr/share/java/calvalus/s1tbx/*.jar /calvalus/software/1.0/S1-L1-GRD-Terrain-Correction-1.0/
+hdfs dfs -copyFromLocal -f processor-bundles/S1-L1-GRD-Terrain-Correction-1.0 /calvalus/software/1.0/
+hdfs dfs -copyFromLocal -f /build/usr/share/java/calvalus/s1tbx/*.jar /calvalus/software/1.0/S1-L1-GRD-Terrain-Correction-1.0/
 
-hdfs dfs -copyFromLocal processor-bundles/S1-L2-OCN-preprocess-1.0 /calvalus/software/1.0/
-hdfs dfs -copyFromLocal /build/usr/share/java/calvalus/calmar/calmar-ocean-wind-energy-op-0.1.0-SNAPSHOT.jar /calvalus/software/1.0/S1-L2-OCN-preprocess-1.0/
+hdfs dfs -copyFromLocal -f processor-bundles/S1-L2-OCN-preprocess-1.0 /calvalus/software/1.0/
+hdfs dfs -copyFromLocal -f /build/usr/share/java/calvalus/calmar/calmar-ocean-wind-energy-op-0.1.0-SNAPSHOT.jar /calvalus/software/1.0/S1-L2-OCN-preprocess-1.0/
