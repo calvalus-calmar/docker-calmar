9ef12faeceaad37497b845a57d0503aaef6215b8
Declan Dunne <d.dunne@ucc.ie>
2021-08-01 14:20:41 +0100

    Update SNAP version

--- calvalus-base/config/scripts/setup_calvalus.sh
+++ calvalus-base/config/scripts/setup_calvalus.sh
@@ -11,12 +11,12 @@ java -jar calvalus/calvalus-distribution/cpt.jar -e --deploy calvalus/calvalus-b
 hdfs dfs -copyFromLocal -f gdal.jar /calvalus/software/1.0/calvalus-2.22
 
 # install snap-bundle
-java -jar calvalus/calvalus-distribution/cpt.jar -e --deploy calvalus/snap-bundle/snap-all.jar snap-8.0.3.3cv
+java -jar calvalus/calvalus-distribution/cpt.jar -e --deploy calvalus/snap-bundle/snap-all.jar snap-8.0.3.4cv
 
 # add snap python support
-hdfs dfs -copyFromLocal -f calvalus/snap-python/org-esa-snap-snap-python.jar /calvalus/software/1.0/snap-8.0.3.3cv
+hdfs dfs -copyFromLocal -f calvalus/snap-python/org-esa-snap-snap-python.jar /calvalus/software/1.0/snap-8.0.3.4cv
 
 # add snap built-in bundle descriptor
-hdfs dfs -copyFromLocal -f calvalus/snap-bundle/resources/bundle-descriptor.xml /calvalus/software/1.0/snap-8.0.3.3cv
+hdfs dfs -copyFromLocal -f calvalus/snap-bundle/resources/bundle-descriptor.xml /calvalus/software/1.0/snap-8.0.3.4cv
 
 echo "Finished installing Calvalus on HDFS"
