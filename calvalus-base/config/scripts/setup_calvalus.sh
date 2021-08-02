cde1998a9be82c11092256d3f1b5bbdf94772dc8
Declan Dunne <d.dunne@ucc.ie>
2021-08-02 16:10:38 +0100

    Add missing JAR for snap-python on Calvalus

--- calvalus-base/config/scripts/setup_calvalus.sh
+++ calvalus-base/config/scripts/setup_calvalus.sh
@@ -3,8 +3,9 @@
 echo "Installing Calvalus to HDFS..."
 hdfs dfs -mkdir -p /calvalus/software/1.0
 
-# install calvalus-bundle
 cd /build/usr/share/java/
+
+# install calvalus-bundle
 java -jar calvalus/calvalus-distribution/cpt.jar -e --deploy calvalus/calvalus-bundle/*.jar calvalus-2.22
 
 # Add GDAL JNI support
@@ -13,8 +14,8 @@ hdfs dfs -copyFromLocal -f gdal.jar /calvalus/software/1.0/calvalus-2.22
 # install snap-bundle
 java -jar calvalus/calvalus-distribution/cpt.jar -e --deploy calvalus/snap-bundle/snap-all.jar snap-8.0.3.4cv
 
-# add snap python support
-hdfs dfs -copyFromLocal -f calvalus/snap-python/org-esa-snap-snap-python.jar /calvalus/software/1.0/snap-8.0.3.4cv
+# install snap-python
+hdfs dfs -copyFromLocal -f calvalus/snap-python/*.jar /calvalus/software/1.0/snap-8.0.3.4cv
 
 # add snap built-in bundle descriptor
 hdfs dfs -copyFromLocal -f calvalus/snap-bundle/resources/bundle-descriptor.xml /calvalus/software/1.0/snap-8.0.3.4cv
