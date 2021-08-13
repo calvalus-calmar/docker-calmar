dd9804235a82e9fe2b07e4a26532ffdca67ee231
Declan Dunne <d.dunne@ucc.ie>
2021-08-13 15:16:13 +0100

    Rename S1-L2-OCN-preprocess processor rename to S1_L2_OCN_restructure_geocoding

--- calvalus-base/config/scripts/setup_processors.sh
+++ calvalus-base/config/scripts/setup_processors.sh
@@ -7,5 +7,5 @@ hdfs dfs -mkdir -p /calvalus/software/1.0
 hdfs dfs -copyFromLocal -f processor-bundles/S1-L1-GRD-Terrain-Correction-1.0 /calvalus/software/1.0/
 hdfs dfs -copyFromLocal -f /build/usr/share/java/calvalus/s1tbx/*.jar /calvalus/software/1.0/S1-L1-GRD-Terrain-Correction-1.0/
 
-hdfs dfs -copyFromLocal -f processor-bundles/S1-L2-OCN-preprocess-1.0 /calvalus/software/1.0/
-hdfs dfs -copyFromLocal -f /build/usr/share/java/calvalus/calmar/calmar-ocean-wind-energy-op-0.1.0-SNAPSHOT.jar /calvalus/software/1.0/S1-L2-OCN-preprocess-1.0/
+hdfs dfs -copyFromLocal -f processor-bundles/S1_L2_OCN_restructure_geocoding-1.0 /calvalus/software/1.0/
+hdfs dfs -copyFromLocal -f /build/usr/share/java/calvalus/calmar/s1-l2-ocn-restructure-geocoding-0.1.0.jar /calvalus/software/1.0/S1_L2_OCN_restructure_geocoding-1.0/
