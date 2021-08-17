ebc358626dbe42f78d3c4bc490eb89e74db43550
Declan Dunne <d.dunne@ucc.ie>
2021-08-17 22:08:48 +0100

    Tidy up S1-L1-GRD-terrain-correction processor

--- calvalus-base/config/scripts/setup_processors.sh
+++ calvalus-base/config/scripts/setup_processors.sh
@@ -4,8 +4,8 @@ echo "Installing demo processors to HDFS..."
 hdfs dfs -mkdir -p /calvalus/software/1.0
 
 # install processors
-hdfs dfs -copyFromLocal -f processor-bundles/S1-L1-GRD-Terrain-Correction-1.0 /calvalus/software/1.0/
-hdfs dfs -copyFromLocal -f /build/usr/share/java/calvalus/s1tbx/*.jar /calvalus/software/1.0/S1-L1-GRD-Terrain-Correction-1.0/
+hdfs dfs -copyFromLocal -f processor-bundles/S1-L1-GRD-terrain-correction-1.0 /calvalus/software/1.0/
+hdfs dfs -copyFromLocal -f /build/usr/share/java/calvalus/s1tbx/*.jar /calvalus/software/1.0/S1-L1-GRD-terrain-correction-1.0/
 
 hdfs dfs -copyFromLocal -f processor-bundles/S1-L2-OCN-restructure-geocoding-1.0 /calvalus/software/1.0/
 hdfs dfs -copyFromLocal -f /build/usr/share/java/calvalus/calmar/s1-l2-ocn-restructure-geocoding-op-0.1.0.jar /calvalus/software/1.0/S1-L2-OCN-restructure-geocoding-1.0/
