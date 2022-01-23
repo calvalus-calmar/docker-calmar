23c1f46fd17dd3865d0db283bcdc43207f0c6e35
Declan Dunne <d.dunne@ucc.ie>
2022-01-23 14:29:22 +0000

    set Calvalus version to 2.23, update the Calvalus build

--- calvalus-base/config/scripts/setup_calvalus.sh
+++ calvalus-base/config/scripts/setup_calvalus.sh
@@ -6,10 +6,10 @@ hdfs dfs -mkdir -p /calvalus/software/1.0
 cd /build/usr/share/java/
 
 # install calvalus-bundle
-java -jar calvalus/calvalus-distribution/cpt.jar -e --deploy calvalus/calvalus-bundle/*.jar calvalus-2.22
+java -jar calvalus/calvalus-distribution/cpt.jar -e --deploy calvalus/calvalus-bundle/*.jar calvalus-2.23
 
 # Add GDAL JNI support
-hdfs dfs -copyFromLocal -f gdal.jar /calvalus/software/1.0/calvalus-2.22
+hdfs dfs -copyFromLocal -f gdal.jar /calvalus/software/1.0/calvalus-2.23
 
 # install snap-bundle
 java -jar calvalus/calvalus-distribution/cpt.jar -e --deploy calvalus/snap-bundle/snap-all.jar snap-8.0.3.6cv
