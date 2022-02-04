b3961e0860719ccefb15140d44c8ebf0fd2c6c12
Declan Dunne <d.dunne@ucc.ie>
2022-02-04 12:30:29 +0000

    update user colour palettes for Sentinel-1 SAR GRD

--- calvalus-base/config/scripts/setup_user_color_palettes.sh
+++ calvalus-base/config/scripts/setup_user_color_palettes.sh
@@ -2,4 +2,8 @@
 
 echo "Copy user-defined color palettes to HDFS..."
 hdfs dfs -mkdir -p /calvalus/auxiliary/cpd
-hdfs dfs -copyFromLocal -f palettes/user_color_palettes/sar.cpd /calvalus/auxiliary/cpd/
+hdfs dfs -copyFromLocal -f palettes/user_color_palettes/gradient_grey.cpd /calvalus/auxiliary/cpd/
+hdfs dfs -copyFromLocal -f palettes/user_color_palettes/s1_sar_grd_ampitude_vh.cpd
+hdfs dfs -copyFromLocal -f palettes/user_color_palettes/s1_sar_grd_ampitude_vv.cpd
+hdfs dfs -copyFromLocal -f palettes/user_color_palettes/s1_sar_grd_intensity_vh.cpd
+hdfs dfs -copyFromLocal -f palettes/user_color_palettes/s1_sar_grd_intensity_vv.cpd
