680c92bf0347599357e81513f2beeff2b7695111
Declan Dunne <d.dunne@ucc.ie>
2022-02-06 22:25:10 +0000

    fix bug when copying colour palettes to HDFS

--- calvalus-base/config/scripts/setup_user_color_palettes.sh
+++ calvalus-base/config/scripts/setup_user_color_palettes.sh
@@ -3,7 +3,7 @@
 echo "Copy user-defined color palettes to HDFS..."
 hdfs dfs -mkdir -p /calvalus/auxiliary/cpd
 hdfs dfs -copyFromLocal -f palettes/user_color_palettes/gradient_grey.cpd /calvalus/auxiliary/cpd/
-hdfs dfs -copyFromLocal -f palettes/user_color_palettes/s1_sar_grd_ampitude_vh.cpd
-hdfs dfs -copyFromLocal -f palettes/user_color_palettes/s1_sar_grd_ampitude_vv.cpd
-hdfs dfs -copyFromLocal -f palettes/user_color_palettes/s1_sar_grd_intensity_vh.cpd
-hdfs dfs -copyFromLocal -f palettes/user_color_palettes/s1_sar_grd_intensity_vv.cpd
+hdfs dfs -copyFromLocal -f palettes/user_color_palettes/s1_sar_grd_ampitude_vh.cpd /calvalus/auxiliary/cpd/
+hdfs dfs -copyFromLocal -f palettes/user_color_palettes/s1_sar_grd_ampitude_vv.cpd /calvalus/auxiliary/cpd/
+hdfs dfs -copyFromLocal -f palettes/user_color_palettes/s1_sar_grd_intensity_vh.cpd /calvalus/auxiliary/cpd/
+hdfs dfs -copyFromLocal -f palettes/user_color_palettes/s1_sar_grd_intensity_vv.cpd /calvalus/auxiliary/cpd/
